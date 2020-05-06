31911617f92d892f429e98c9e6289955e9e504d4
chajinjoo <chajanee@gmail.com>
2020-05-06 12:27:43 +0900

    feat: 초기세팅 & Home 레이아웃 구성
    - 초기세팅
    - Home 페이지 백그라운드 스크롤 이미지 구성
    
    https://trello.com/c/JkAUI0xL

---
+++ src/components/NavBar.vue
@@ -0,0 +1,45 @@
+<template>
+   <div>
+      <nav>
+         <ul>
+            <li><a href="#">Pricing</a></li>
+            <li><a href="#">Portfolio</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">이거는 nav다ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ</a></li>
+         </ul>
+      </nav>
+   </div>
+</template>
+
+<script>
+   export default {
+      name: "NavBar"
+   }
+</script>
+
+<style scoped lang="scss">
+   nav{
+      width: 100%;
+      height: 40px;
+      ul {
+         display: flex;
+         justify-content: space-around;
+         li{
+            list-style-type: none;
+            float: right;
+            line-height: 50px;
+            padding-right: 4.8%;
+            height: 50px;
+            a{
+               text-decoration: none;
+               color: black;
+            }
+            a:hover{
+               color: green;
+            }
+         }
+      }
+   }
+
+</style>

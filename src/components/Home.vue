62b4c7bad8beb817631aaaaba3c97648e0a19fb2
chajinjoo <chajanee@gmail.com>
2020-05-07 16:06:47 +0900

    feat: Home 페이지 About Us 부분 구현
    - 백그라운드 및 폰트 style 미적용
    - "Noto sans" 웹폰트 추가
    
    https://trello.com/c/0yikWylb

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,10 +3,9 @@
     <div class="backgroundDiv one">
       <div>
         <div class="captionContainer">
-          <div class="captionTitle">지인, 개발자, 상사에게 물어보지 마세요.
-            실제 게이머에게 물어보세요.</div>
-          <div class="captionBody">예측은 그만! 게이머의 마음을 정확히 알고 싶다면,
-            게이머에게 물어봐야 합니다.</div>
+          <div class="captionTitle">지인에게 물어보지 마세요.<br>
+            게이머에게 양보하세요.</div>
+          <div class="captionBody">게이머의 마음을 정확히 알고 싶다면, 직접 물어봐야 합니다.</div>
           <section>
             <div class="buttons">
               <button class="button is-medium is-warning" @click="warning">
@@ -25,34 +24,45 @@
     </div>
     <div class="majorGameSubContainer">
       <div>
-        <p class="majorGameSubTitle">버프스튜디오 김도형 대표님</p>
-        <div class="divisionLine"></div>
-        <p class="majorGameSubBody">포메이커스, 너무 만족했습니다.
-          게임에 대해 유저 분들의 퀄리티 좋은 피드백을 받아 게임을
-          개선하고 싶다면 추천드려요!!</p>
         <img src="../assets/images/buf_logo.png" alt="">
+        <p class="majorGameSubTitle">버프스튜디오 김도형 대표님</p>
+        <p class="majorGameSubBody">포메이커스, 너무 만족했습니다.<br>
+          게임에 대한 퀄리티 좋은 유저 피드백을 받아
+          게임을 개선하고 싶다면 추천드려요!!</p>
+        <div class="divisionLine"></div>
       </div>
       <div>
+        <img src="../assets/images/wonder_logo.png" alt="">
         <p class="majorGameSubTitle" style="color: #EDAE30">원더스쿼드 서관희 대표님</p>
-        <div class="divisionLine" style="background-color: #EDAE30"></div>
-        <p class="majorGameSubBody">솔직하고 정성스럽게 써주신 분들이 많아서
+        <p class="majorGameSubBody">솔직하고 정성스럽게 써주신 분들이 많아서 <br>
           단지 게임 평점에서는 알 수 없었던 속마음과
           좋은 의견들을 들을 수 있어서 좋았습니다.</p>
-        <img src="../assets/images/wonder_logo.png" alt="">
+        <div class="divisionLine" style="background-color: #EDAE30"></div>
       </div>
       <div>
+        <img src="../assets/images/zabob_logo.png" alt="">
         <p class="majorGameSubTitle" style="color: #EE8292">자밥 스튜디오 권대현 대표님</p>
-        <div class="divisionLine" style="background-color: #EE8292"></div>
-        <p class="majorGameSubBody">스토어 리뷰나 지인 평가는 참고만 해야지,
+        <p class="majorGameSubBody">스토어 리뷰나 지인 평가는 참고만 해야지, <br>
           믿을게 못됩니다. 이 서비스를 통해 내 게임의 반응을
           적나라하게 파악할 수 있었습니다.</p>
-        <img src="../assets/images/zabob_logo.png" alt="">
+        <div class="divisionLine" style="background-color: #EE8292"></div>
       </div>
     </div>
 
     <div class="backgroundDiv two">
-      <div class="captionTitle">1</div>
-
+      <div class="backgroundColor"></div>
+      <div>
+        <div class="aboutContainer">
+          <div class="aboutTitle">About Us</div>
+          <div class="eveImgBox">
+            <img src="../assets/images/eve_img.png" alt="">
+          </div>
+          <div class="aboutHeader">" 사용자 의견은 점점 중요해지는데, <br>왜 듣는 방법은 여전히 원시적일까요? "</div>
+          <div class="aboutBody">저는 전세계 사용자를 대상으로 제품을 만들던 UX 전문가였습니다. <br>매년 개발 툴이 쏟아져도 사용자 의견 수집 방법은 늘 똑같았습니다. <br><div style="font-weight: bold;">느리거나, 비싸거나, 복잡하거나.</div></div>
+          <div class="aboutBody">사용자 의견을 더 합리적으로 들을 수 있어야 한다고 생각했습니다. <br>작은 기업의 위험성을 줄여주고, 큰 기업의 리소스를 아껴주고 싶었습니다. <br>이 사업을 통해 기업들이 본질에 집중할 수 있게 돕고 싶습니다.</div>
+          <div class="aboutBody"> - 포메이커스 대표 이보림 올림-</div>
+        </div>
+      </div>
     </div>
     <div class="contentBox">
       <p>본문</p>

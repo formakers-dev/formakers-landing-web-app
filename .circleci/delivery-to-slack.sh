fa53a5de511da4159eaf60d51a2cb9f437d3a76e
yenarue <yenarue@gmail.com>
2020-05-13 22:17:31 +0900

    chore: CircleCI - Slack Delivery 문구 변경
    https://trello.com/c/XSdnVUmV

--- .circleci/delivery-to-slack.sh
+++ .circleci/delivery-to-slack.sh
@@ -23,7 +23,7 @@ fi
 NEW_LINE=$'\n'
 
 curl -F channel=$2 \
-    -F text="[$1] B2B 랜딩페이지 $VERSION 버전이 릴리즈 되었습니다!${NEW_LINE}:point_right::skin-tone-2: <$URL|운영페이지 확인하러 가기>" \
+    -F text="[$1] B2B 랜딩페이지 $VERSION 버전이 릴리즈 되었습니다!${NEW_LINE}:point_right::skin-tone-2: <$URL|랜딩페이지 확인하러 가기>" \
     -F token=$SLACK_BOT_TOKEN \
     -F as_user=true \
     https://slack.com/api/chat.postMessage

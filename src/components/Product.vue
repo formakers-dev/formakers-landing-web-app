c114ac20311c28d19328afb17375635af2f26608
chajinjoo <chajanee@gmail.com>
2020-06-10 12:18:02 +0900

    feat: 플랜 공통 컴포넌트 스타일 수정 완료 (스탠다드 기준)
    - select box 데이터 매핑 적용예정
    - 조건분기 적용예정
    
    https://trello.com/c/5jWtJ6PP

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -3,9 +3,9 @@
     <div class="menuBackground"></div>
 
     <div class="productContainer">
-      <div style="display: flex">
+      <div style="display: flex; justify-content: center; padding: 0 40px">
         <div class="productBox">
-          <div style="padding: 40px 0">
+          <div>
             <p class="productTitle">
               {{ data.title }}
             </p>
@@ -25,12 +25,6 @@
               }}</label>
               <select id="selected1" v-model="selected1">
                 <option disabled value="">선택하세요.</option>
-                <!--              <option>-->
-                <!--                {{ data.options[0].data[0].label }}-->
-                <!--              </option>-->
-                <!--              <option>-->
-                <!--                {{ data.options[0].data[1].label }}-->
-                <!--              </option>-->
                 <option>10명 추가 (+500,000원)</option>
                 <option>20명 추가 (+1,000,000원)</option>
                 <option>인원 추가 안함</option>
@@ -87,36 +81,222 @@
             <div style="height: 100px"></div>
 
             <!-- data 테스트 -->
-            <!--                    <h1>{{ data }}</h1>-->
+            <!--   <h1>{{ data }}</h1>-->
           </div>
         </div>
 
         <!-- 이미지 탭 -->
         <div class="productImgContainer">
           <div class="productTitleImgBox">
-            <img src="../assets/images/standard.png" alt="" />
+            <img ref="titleImg" src="../assets/images/standard.png" alt="" />
           </div>
 
           <div class="productImgBox" v-if="data.productImages">
-            <img
+            <div
               v-for="(images, index) in data.productImages"
               v-bind:key="index"
-              :src="images"
-              alt=""
-              v-on:click="clickImg"
-            />
-            {{ images }}
+            >
+              <img :src="images" alt="" @click="clickImg(images)" />
+              <!--   {{ images }}-->
+            </div>
           </div>
         </div>
       </div>
 
       <!-- contents -->
-      <div class="contentsContainer">
-        <div>
+      <div
+        class="contentsContainer"
+        style="padding: 100px 40px; width: 800px; margin: 0 auto; background-color: #333333; text-align: center;"
+      >
+        <div style="display: flex; justify-content: start; margin-bottom: 40px">
           <img
+            style="width: 600px;"
             src="https://contents.sixshop.com/thumbnails/uploadedFiles/107454/product/image_1587458446255_1000.png"
             alt=""
           />
+        </div>
+
+        <div style="text-align: start">
+          <div style="margin: 50px 0">
+            <span
+              class="planTitle"
+              style="background-color: #1abc9c; letter-spacing: 0.02em; line-height: 2em; font-size: 24px; font-weight: bold; color: #000000;"
+            >
+              Standard 플랜 주요 안내
+            </span>
+            <p class="planSub" style="color: #ffffff; line-height: 2em">
+              * 이 플랜은
+              <strong style="color: #ffffff; text-decoration: none"
+                >구글플레이에
+              </strong>
+              <strong style="color: #e74c3c; text-decoration: underline;"
+                >출시된 게임</strong
+              >과
+              <span style="color: #e74c3c; text-decoration: underline;"
+                ><strong style="color: #e74c3c; text-decoration: underline;"
+                  >미출시 게임</strong
+                >
+                모두 이용 가능</span
+              >합니다.
+              <br />
+              * 미출시 게임의 경우 옵션에서
+              <span style="color: #e74c3c; text-decoration: none"
+                ><strong style="color: #e74c3c; text-decoration: underline;"
+                  >"클로즈 베타 테스트 진행"을 선택</strong
+                >하여 구매</span
+              >해주세요. <br />
+              * 인원추가를 원하실 경우
+              <span style="color: #e74c3c; text-decoration: none"
+                >옵션에서
+                <strong style="color: #e74c3c; text-decoration: underline;"
+                  >"10명추가" 혹은 "20명 추가"를 선택</strong
+                >하여 구매</span
+              >해주세요. <br />
+            </p>
+          </div>
+
+          <div style="margin: 50px 0">
+            <span
+              class="planTitle"
+              style="background-color: #1abc9c; letter-spacing: 0.02em; line-height: 2em; font-size: 24px; font-weight: bold; color: #000000;"
+            >
+              Standard 플랜 샘플 보기
+            </span>
+            <div>
+              <a
+                class="sampleLink"
+                href="https://forms.gle/FPTboW8gYbpd3fSH8"
+                target="_blank"
+              >
+                <span>1. Standard 설문 샘플 (클릭)</span>
+              </a>
+            </div>
+            <div>
+              <a
+                class="sampleLink"
+                href="https://drive.google.com/file/d/1mamYs1Y-XKSj107nSpMmVIvzuPNKyjg4/view"
+                target="_blank"
+              >
+                <span>2. Standard 결과 시트 샘플 (클릭)</span>
+              </a>
+            </div>
+            <div>
+              <a
+                class="sampleLink"
+                href="https://docs.google.com/spreadsheets/u/1/d/1JNnVrBGN3tr397mTptaA9Si-ZpFqWWaPghbtN4U274Q/edit?usp=sharing"
+                target="_blank"
+              >
+                <span>3. 유저 데이터 샘플 (클릭)</span>
+              </a>
+            </div>
+          </div>
+
+          <div style="margin: 50px 0">
+            <span
+              class="planTitle"
+              style="background-color: #1abc9c; letter-spacing: 0.02em; line-height: 2em; font-size: 24px; font-weight: bold; color: #000000;"
+            >
+              Standard 플랜 FAQ
+            </span>
+            <p
+              class="planSub"
+              style="color: #ffffff; line-height: 2em; font-weight: bold"
+            >
+              Q. 심플 플랜과 스탠다드 플랜의 차이점은 무엇인가요?
+            </p>
+            <p class="planSub" style="color: #ffffff; line-height: 2em">
+              A. 가장 큰 차이는 더 많은 카테고리의 의견을 들을 수 있고, 유저
+              데이터를 제공한다는 점입니다. 특히 클로즈 베타테스트가 가능하기
+              때문에 원하실 경우 추가구매가 가능합니다. (클로즈 베타 테스트가
+              필요없는 경우 옵션 선택 X)
+            </p>
+          </div>
+
+          <div
+            style="border-left: 5px solid #ccc; margin: 18px 20px; padding: 2px 20px;"
+          >
+            <strong>
+              <span
+                style="background-color: #000000; font-style: italic; color: #f39c12;"
+              >
+                심플 플랜 카테고리
+              </span>
+            </strong>
+
+            <p
+              style="color: #f39c12; font-style: italic; line-height: 2em; letter-spacing: 0.02em;"
+            >
+              : 참여 정보, 재미 점수, 이탈 의향, 플레이 지속 의향, 흥미요소,
+              부정적 요소, 첫인상&튜토리얼, 난이도, 광고 및 과금, 이탈포인트,
+              유지할 점, 개선할 점, 게임사에 하고싶은 말
+            </p>
+            <br />
+            <br />
+
+            <strong>
+              <span
+                style="background-color: #000000; font-style: italic; color: #1abc9c;"
+              >
+                심플 플랜 카테고리
+              </span>
+            </strong>
+
+            <p
+              style="color: #1abc9c; font-style: italic; line-height: 2em; letter-spacing: 0.02em;"
+            >
+              : 참여 정보, 재미 점수, 이탈 의향, 플레이 지속 의향, 흥미요소,
+              부정적 요소, 첫인상&튜토리얼, 난이도, 광고 및 과금, 이탈포인트,
+              유지할 점, 개선할 점, 게임사에 하고싶은 말
+            </p>
+          </div>
+
+          <div style="margin: 50px 0">
+            <p
+              class="planSub"
+              style="color: #ffffff; line-height: 2em; font-weight: bold"
+            >
+              Q. 스탠다드 플랜은 어떤 게임사에게 추천하나요?
+            </p>
+            <p class="planSub" style="color: #ffffff; line-height: 2em;">
+              A. 유저의 실제 데이터와 유저가 왜 그렇게 행동하는지 모두를 알고
+              싶은 게임사에게 추천합니다. 정성조사와 정량조사를 한번에 진행하여
+              디테일한 분석을 하고 싶으신 경우 추천드립니다.
+            </p>
+          </div>
+
+          <div style="margin: 50px 0">
+            <p
+              class="planSub"
+              style="color: #ffffff; line-height: 2em; font-weight: bold"
+            >
+              Q. 몇명의 결과를 얻을 수 있나요? 10명, 20명을 추가하는게 좋은가요?
+            </p>
+            <p class="planSub" style="color: #ffffff; line-height: 2em;">
+              A. 게임사마다 원하시는 인원은 다릅니다만, 피드백 결과의 신뢰성
+              확보를 위해 30명 이상은 꼭 하시기를 추천드립니다. 제공해드리는
+              응답 중 불성실 응답자는 제외해서 결과서를 제공합니다. (무성의한
+              응답이나 욕설 등)
+            </p>
+          </div>
+
+          <!-- 플랜 선택 -->
+          <div class="choicePlan">
+            <p>
+              <span
+                ><strong
+                  >혹시, 어떤 플랜을 선택해야 할지 고민되시나요?</strong
+                ></span
+              >
+            </p>
+            <p>
+              <span
+                ><strong
+                  >오른쪽 하단의 무료 채팅 상담을 진행해 보세요 :-)
+                </strong></span
+              >
+            </p>
+            <p>(채팅 상담 시간 : 평일 11시 ~ 19시)</p>
+          </div>
         </div>
       </div>
     </div>
@@ -134,7 +314,7 @@ export default {
       selected2: "",
       selected3: "",
       selectTotal: false,
-      images: ""
+      images: "",
     };
   },
   filters: {
@@ -158,8 +338,9 @@ export default {
     buyTotal() {
       this.selectTotal = true;
     },
-    clickImg() {
-      console.log(this.images);
+    clickImg(images) {
+      // console.log(images)
+      this.$refs.titleImg.src = images;
     }
   }
 };
@@ -168,7 +349,7 @@ export default {
 <style scoped lang="scss">
 //메뉴 백그라운드
 .menuBackground {
-  padding-top: 55px;
+  padding-top: 100px;
   padding-bottom: 50px;
   background-color: #333333;
 }
@@ -177,13 +358,10 @@ export default {
   width: 100%;
   margin: 0 auto;
   background-color: #333333;
-  padding: 100px;
   .productBox {
     width: 100%;
-    display: flex;
-    justify-content: center;
-    margin: 0 auto;
     background-color: #333333;
+    max-width: 500px;
     .productTitle {
       color: #ffffff;
       font-weight: 700;
@@ -216,7 +394,6 @@ export default {
   display: block;
   margin-top: 35px;
 }
-
 .selectTotalBox {
   width: 450px;
 }
@@ -272,31 +449,6 @@ export default {
   }
 }
 
-.productImgContainer {
-  width: 100%;
-  margin: 0 auto;
-  background-color: #333333;
-  .productTitleImgBox {
-    width: 60%;
-    /*height: 50%;*/
-    img {
-      width: 500px;
-      height: 500px;
-      padding: 10px;
-    }
-  }
-  .productImgBox {
-    width: 60%;
-    height: 100%;
-    img {
-      width: 150px;
-      height: 150px;
-      margin: 10px;
-      cursor: pointer;
-    }
-  }
-}
-
 select {
   background-color: #333333;
   color: #ffffff;
@@ -319,9 +471,47 @@ input {
   }
 }
 
-// contents
-.contentsContainer {
-  display: flex;
-  justify-content: center;
+// 이미지 프리뷰
+.productImgContainer {
+  max-width: 500px;
+  margin-bottom: 200px;
+  .productTitleImgBox {
+    img {
+    }
+  }
+  .productImgBox {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+
+    img {
+      width: 155px;
+      margin-left: 0;
+      margin-right: 10px;
+      margin-top: 20px;
+      cursor: pointer;
+    }
+  }
+}
+
+.sampleLink {
+  color: #3498db;
+  text-decoration: underline;
+}
+
+// 플랜 선
+.choicePlan {
+  padding: 50px 0;
+  span > strong {
+    color: #ffffff;
+    letter-spacing: 0.02em;
+    line-height: 2em;
+    font-size: 24px;
+  }
+  p {
+    color: #ffffff;
+    letter-spacing: 0.02em;
+    line-height: 2em;
+  }
 }
 </style>

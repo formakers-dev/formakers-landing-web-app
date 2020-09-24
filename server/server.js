c1d36c154d291414bc5e14e46ab20912ac4ad020
Rosie <rosie.yang@formakers.net>
2020-09-24 21:43:50 +1000

    feat: MongoDB 연결
    - 서비스DB에 연결
    
    https://trello.com/c/cf27HWKZ

--- server/server.js
+++ server/server.js
@@ -2,6 +2,21 @@ const mongoose = require('mongoose');
 
 const app = require('./app');
 
+// ===== Connect to DB =====
+const DB = process.env.MONGODB_URL.replace(
+  '<password>',
+  process.env.MONGODB_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('MongoDB connected successfully👍'));
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {

5a3fec5c5b121b9ba076e574a3a4f341743861ba
Rosie <rosie.yang@formakers.net>
2020-09-28 15:02:34 +1000

    feat: Home에 Title, SearchForm 컴포넌트 연결
    https://trello.com/c/M2zU7NvB

---
+++ client/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home-page">
+    <Title />
+    <SearchForm />
+  </div>
+</template>
+
+<script>
+import Title from '@/components/Title';
+import SearchForm from '@/components/SearchForm';
+
+export default {
+  name: 'Home',
+  components: {
+    Title,
+    SearchForm
+  }
+};
+</script>
+
+<style scoped>
+
+</style>

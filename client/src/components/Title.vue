59ba3573428428c3525cb810dab2fae001830298
Rosie <rosie.yang@formakers.net>
2020-09-28 15:03:14 +1000

    feat: Title 컴포넌트 추가
    https://trello.com/c/M2zU7NvB

--- client/src/components/Title.vue
+++ client/src/components/Title.vue
@@ -1,22 +1,22 @@
 <template>
   <section class="title">
     <h1 class="title is-1">Persona</h1>
-    <h3 class="subtitle is-4">당신이 원하는 유저를 빠르고 쉽게 만나보세요!</h3>
+    <h3 class="subtitle is-4">당신이 원하는 게이머를 빠르고 쉽게 만나보세요!</h3>
   </section>
 </template>
 
 <script>
 export default {
-  name: 'Title'
-}
+  name: 'Title',
+};
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
 
-.title.is-1 {
-  font-family: 'Pacifico', cursive;
-  font-size: 5rem;
-  margin-bottom: 3rem;
-}
+  .title.is-1 {
+    font-family: 'Pacifico', cursive;
+    font-size: 5rem;
+    margin: 7rem 0 3rem;
+  }
 </style>

6f042dcfbde9d3ae8af97e6644af3e6ada22b1a3
Rosie <rosie.yang@formakers.net>
2020-09-28 16:25:15 +1000

    feat: SearchForm 컴포넌트 추가
    - Button 클릭시 상세 필터링 옵션 박스 보여주기
    
    https://trello.com/c/M2zU7NvB

---
+++ client/src/components/SearchForm.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="search-wrapper">
+    <b-button
+        rounded
+        class="initial-search"
+        @click="isExpanded = !isExpanded"
+        v-show="!isExpanded">
+      🔍 원하는 게이머 타입을 선택하세요
+    </b-button>
+
+    <div class="box expanded-search" v-show="isExpanded">
+      <!--검색 필터링옵션 넣기-->
+      <p class="control">
+        <b-button class="button is-dark" expanded>찾 기</b-button>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchForm',
+  data() {
+    return {
+      isExpanded: false,
+    }
+  }
+};
+</script>
+
+<style scoped>
+  .expanded-search {
+    width: 60vw;
+    margin: 0 auto;
+  }
+
+  .button.is-dark {
+    width: 10rem;
+    margin: 0 auto;
+  }
+</style>

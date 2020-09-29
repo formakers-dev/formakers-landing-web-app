0d53fed5dbeb413e9b07d534f2a9e6edcb3c632f
Rosie <rosie.yang@formakers.net>
2020-09-29 12:47:33 +1000

    feat: Error 핸들링
    - Uncaught Exception 에러
    - Unhandled Rejection 에러
    
    https://trello.com/c/sE9AtPaj

--- server/server.js
+++ server/server.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 
+// ===== Error Handler (Uncaught Exception) =====
+process.on('uncaughtException', err => {
+  console.log('💥 [Uncaught Exception] Shutting down... 💥');
+  console.log(`${err.name}: ${err.message}`);
+  process.exit(1);
+});
+
 const app = require('./app');
 
 // ===== Connect to DB =====
@@ -19,6 +26,15 @@ mongoose
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`🎉 Server running in ${process.env.NODE_ENV.toUpperCase()} mode on port ${PORT} 🎉`);
+});
+
+// ===== Error Handler (Unhandled Rejection) =====
+process.on('unhandledRejection', err => {
+  console.log('💥 [Unhandled Rejection] Shutting down... 💥');
+  console.log(`${err.name}: ${err.message}`);
+
+  // Close server & exit process
+  server.close(() => process.exit(1));
 });

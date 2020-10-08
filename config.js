d535c3666e113430580fcbec5b78ab43042cd5d6
Rosie <rosie.yang@formakers.net>
2020-10-08 11:51:49 +1100

    feat: config 파일 추가
    https://trello.com/c/M2zU7NvB

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {};
+
+config.development = {
+  serverBaseUrl: process.env.VUE_APP_SERVER_BASE_URL || "http://localhost:8080",
+  port: process.env.VUE_APP_PORT || 3000
+};
+
+config.staging = config.development;
+config.production = config.development;
+
+module.exports = config[process.env.NODE_ENV];

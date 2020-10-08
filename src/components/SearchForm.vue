4e924ced186fa3b655e867e2ba4a2155eb094ed3
Rosie <rosie.yang@formakers.net>
2020-10-08 16:24:15 +1100

    feat: 선택된 search 옵션값을 영문으로 저장
    https://trello.com/c/M2zU7NvB

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -1,51 +1,40 @@
 <template>
   <div class="search-wrapper">
     <b-button
-        rounded
-        class="initial-search"
-        @click="isExpanded = !isExpanded"
-        v-show="!isExpanded">
+      rounded
+      class="initial-search"
+      @click="isExpanded = !isExpanded"
+      v-show="!isExpanded"
+    >
       🔍 원하는 게이머 타입을 선택하세요
     </b-button>
 
-    <form
-        class="box expanded-search"
-        v-show="isExpanded"
-    >
-      <div
-          class="block"
-          v-for="(field, index) in fields"
-          :key="index"
-      >
-        <span>{{ field.name }}</span>
+    <section class="box expanded-search" v-show="isExpanded">
+      <div v-for="(field, index) in fields" :key="index">
+        <span>{{ field.name }} : </span>
         <b-checkbox
-            type="is-info"
-            v-for="(option, index) in field.options"
-            v-model="selectedOptions[field.key]"
-            :key="index"
-            :native-value="option"
+          type="is-info"
+          v-for="(option, index) in field.displayText"
+          v-model="selectedOptions[field.key]"
+          :key="index"
+          :native-value="field.options[index]"
         >
           <span>{{ option }}</span>
         </b-checkbox>
       </div>
 
       <p class="control">
-        <b-button
-            class="button is-dark"
-            type="submit"
-            @click="search"
-            expanded
-        >
+        <b-button class="button is-dark" type="submit" @click="search" expanded>
           찾 기
         </b-button>
       </p>
-    </form>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'SearchForm',
+  name: "SearchForm",
   data() {
     return {
       isExpanded: false,
@@ -53,15 +42,79 @@ export default {
         gender: [],
         ageGroup: [],
         playStyle: [],
-        payStyle: [],
+        payStyle: []
       },
       fields: [
-        { name: '성별', key: 'gender', options: ['무관', '남자', '여자'] },
-        { name: '연령대', key: 'ageGroup', options: ['무관', '10대', '20대', '30대', '40대', '50대 이상'] },
-        { name: '게임 플레이 방식', key: 'playStyle', options: ['무관', 'PC', '모바일', '콘솔', '액션', '어드벤처', '아케이드', '보드', '카드', '카지노', '캐주얼 게임', '교육', '음악', '퍼즐', '자동차 경주', '롤플레잉', '시뮬레이션', '스포츠', '전략', '퀴즈', '단어']},
-        { name: '과금 여부', key: 'payStyle', options: ['무관', '과금', '무과금']},
-      ],
-    }
+        {
+          name: "성별",
+          key: "gender",
+          displayText: ["무관", "남자", "여자"],
+          options: ["all", "male", "female"]
+        },
+        {
+          name: "연령대",
+          key: "ageGroup",
+          displayText: ["무관", "10대", "20대", "30대", "40대", "50대 이상"],
+          options: ["all", "10s", "20s", "30s", "40s", "50plus"]
+        },
+        {
+          name: "게임 플레이 방식",
+          key: "playStyle",
+          displayText: [
+            "무관",
+            "PC",
+            "모바일",
+            "콘솔",
+            "액션",
+            "어드벤처",
+            "아케이드",
+            "보드",
+            "카드",
+            "카지노",
+            "캐주얼",
+            "교육",
+            "음악",
+            "퍼즐",
+            "레이",
+            "롤플레잉",
+            "시뮬레이션",
+            "스포츠",
+            "전략",
+            "퀴즈",
+            "단어"
+          ],
+          options: [
+            "all",
+            "pc",
+            "mobile",
+            "console",
+            "action",
+            "adventure",
+            "arcade",
+            "board",
+            "card",
+            "casino",
+            "casual",
+            "educational",
+            "music",
+            "puzzle",
+            "racing",
+            "rolePlaying",
+            "simulation",
+            "sports",
+            "strategy",
+            "trivia",
+            "word"
+          ]
+        },
+        {
+          name: "과금 여부",
+          key: "payStyle",
+          displayText: ["무관", "과금", "무과금"],
+          options: ["all", "pay", "free"]
+        }
+      ]
+    };
   },
   methods: {
     search() {

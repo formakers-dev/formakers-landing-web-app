b95b471bfb19f380280b752516e7ef03721d1ff4
Rosie <rosie.yang@formakers.net>
2020-10-14 15:50:35 +1100

    feat: Vuex의 state와 mutations 추가
    - 검색시 선택한 필터들을 저장하고 모든 컴포넌트에서 접근가능하게 함
    
    https://trello.com/c/M2zU7NvB

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,17 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    selectedOptions: {
+      gender: [],
+      ageGroup: [],
+      playStyle: [],
+      payStyle: []
+    }
+  },
+  mutations: {
+    SET_SELECTED_OPTIONS(state, options) {
+      state.selectedOptions = options;
+    }
+  }
 });

9973c71f148129c7135201b59486fa86e7c05de0
Rosie <rosie.yang@formakers.net>
2020-10-14 16:39:49 +1100

    feat: ShowFilters 컴포넌트 추가
    - 검색화면에서 선택했던 옵션들을 조회화면에서 보여주기
    
    https://trello.com/c/0EmeeFwf

---
+++ src/components/ShowFilters.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="filter-box">
+    <h2>[ 선택한 필터들 ]</h2>
+    <p>성별 : {{ selectedOptions.gender }}</p>
+    <p>연령대 : {{ selectedOptions.ageGroup }}</p>
+    <p>게임 플레이 방식 : {{ selectedOptions.playStyle }}</p>
+    <p>과금 여부 : {{ selectedOptions.payStyle }}</p>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "ShowFilters",
+  computed: {
+    ...mapState(["selectedOptions"])
+  }
+};
+</script>
+
+<style scoped>
+.filter-box {
+  width: 60vw;
+  margin: 2rem auto;
+  padding: 2rem;
+  border: 2px solid grey;
+}
+</style>

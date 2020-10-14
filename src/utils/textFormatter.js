041fad68622590f70826b4138b838f4dea102686
Rosie <rosie.yang@formakers.net>
2020-10-15 10:13:03 +1100

    feat: displayText와 encodedText를 관리하는 textFormatter 생성
    https://trello.com/c/0EmeeFwf

---
+++ src/utils/textFormatter.js
@@ -0,0 +1,66 @@
+export const displayText = {
+  gender: ["무관", "남자", "여자"],
+  ageGroup: [
+    "무관",
+    "10세 미만",
+    "10대",
+    "20대",
+    "30대",
+    "40대",
+    "50대",
+    "60대 이상"
+  ],
+  playStyle: [
+    "무관",
+    "PC",
+    "모바일",
+    "콘솔",
+    "액션",
+    "어드벤처",
+    "아케이드",
+    "보드",
+    "카드",
+    "카지노",
+    "캐주얼",
+    "교육",
+    "음악",
+    "퍼즐",
+    "레이",
+    "롤플레잉",
+    "시뮬레이션",
+    "스포츠",
+    "전략",
+    "퀴즈",
+    "단어"
+  ],
+  payStyle: ["무관", "과금", "무과금"]
+};
+
+export const encodedText = {
+  gender: ["all", "male", "female"],
+  ageGroup: ["all", "under10", "10s", "20s", "30s", "40s", "50s", "over60"],
+  playStyle: [
+    "all",
+    "pc",
+    "mobile",
+    "console",
+    "action",
+    "adventure",
+    "arcade",
+    "board",
+    "card",
+    "casino",
+    "casual",
+    "educational",
+    "music",
+    "puzzle",
+    "racing",
+    "rolePlaying",
+    "simulation",
+    "sports",
+    "strategy",
+    "trivia",
+    "word"
+  ],
+  payStyle: ["all", "pay", "free"]
+};

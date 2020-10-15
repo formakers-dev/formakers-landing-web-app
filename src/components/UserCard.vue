63532e0d89fa863869b13c2977521d1f7fc8f740
Rosie <rosie.yang@formakers.net>
2020-10-15 15:22:37 +1100

    feat: UserCard 컴포넌트에 유저 데이터를 패스해 출력하기
    https://trello.com/c/0EmeeFwf

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -1,12 +1,31 @@
 <template>
   <div class="user-card">
-    <h1>UserCard</h1>
+    <b-checkbox />
+    <h3>{{ index + 1 }}</h3>
+    <p>
+      성별 : {{ user.gender || "-" }} / 출생연도 : {{ user.birthday || "-" }}년
+      / 직업 : {{ user.job || "-" }}
+    </p>
+    <p>선호 플랫폼 : {{ user.favoritePlatforms.join(", ") || "-" }}</p>
+    <p>
+      선호 장르 : {{ user.favoriteGenres.join(", ") || "-" }} / 비선호 장르 :
+      {{ user.leastFavoriteGenres.join(", ") || "-" }}
+    </p>
+    <p>
+      디바이스 : 제조사 - {{ user.device.manufacturer || "-" }}, 모델명 -
+      {{ user.device.model || "-" }}, OS버전 -
+      {{ user.device.osVersion || "-" }}
+    </p>
+    <p>인생게임 : {{ user.lifeApps.join(", ") || "-" }}</p>
+    <p>월 과금규모 : {{ user.monthlyPayment || "-" }}만원</p>
+    <p>연락가능확률 : {{ user.activatedDate || "-" }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: "UserCard"
+  name: "UserCard",
+  props: ["user", "index"]
 };
 </script>
 

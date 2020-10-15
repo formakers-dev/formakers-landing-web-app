fd58721d5ac109a3869c9b43a28a184170e283f2
Rosie <rosie.yang@formakers.net>
2020-10-15 15:18:03 +1100

    feat: 검색시 사용한 필터를 한글로 표시
    https://trello.com/c/0EmeeFwf

--- src/components/ShowFilters.vue
+++ src/components/ShowFilters.vue
@@ -1,20 +1,38 @@
 <template>
   <div class="filter-box">
     <h2>[ 선택한 필터들 ]</h2>
-    <p>성별 : {{ selectedOptions.gender }}</p>
-    <p>연령대 : {{ selectedOptions.ageGroup }}</p>
-    <p>게임 플레이 방식 : {{ selectedOptions.playStyle }}</p>
-    <p>과금 여부 : {{ selectedOptions.payStyle }}</p>
+    <p>성별 : {{ displayFilters.gender.join(", ") || "-" }}</p>
+    <p>연령대 : {{ displayFilters.ageGroup.join(", ") || "-" }}</p>
+    <p>게임 플레이 방식 : {{ displayFilters.playStyle.join(", ") || "-" }}</p>
+    <p>과금 여부 : {{ displayFilters.payStyle.join(", ") || "-" }}</p>
   </div>
 </template>
 
 <script>
 import { mapState } from "vuex";
+import { displayText } from "@/utils/textFormatter";
 
 export default {
   name: "ShowFilters",
+  data() {
+    return {
+      displayFilters: {}
+    };
+  },
   computed: {
     ...mapState(["selectedOptions"])
+  },
+  created() {
+    this.showDisplayText(this.selectedOptions);
+  },
+  methods: {
+    showDisplayText(fields) {
+      Object.keys(fields).forEach(field => {
+        this.displayFilters[field] = fields[field].map(
+          value => displayText[field][value]
+        );
+      });
+    }
   }
 };
 </script>

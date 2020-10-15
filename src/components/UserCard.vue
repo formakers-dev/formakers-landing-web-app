ab47feb40997f4bdf13b361467f98c7c6c0a4938
Rosie <rosie.yang@formakers.net>
2020-10-15 12:52:20 +1100

    feat: UserCard 컴포넌트 template 생성
    https://trello.com/c/0EmeeFwf

---
+++ src/components/UserCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="user-card">
+    <h1>UserCard</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserCard"
+};
+</script>
+
+<style scoped>
+.user-card {
+  width: 70vw;
+  margin: 2rem auto;
+}
+</style>

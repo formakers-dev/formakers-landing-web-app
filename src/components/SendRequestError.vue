7d83e4ea788e8424ddec98b07753e3191920433f
Rosie <rosie.yang@formakers.net>
2020-10-21 00:18:19 +1100

    feat: 요청 전송결과 메세지 보여주기
    - 성공시 SendRequestSuccess
    - 실패시 SendRequestError
    
    https://trello.com/c/Vvv18An2

---
+++ src/components/SendRequestError.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="modal-card">
+    <header class="modal-card-head">
+      <p class="modal-card-title">에러가 발생했습니다. 🥺</p>
+    </header>
+    <div class="modal-card-body">
+      <p>이용에 불편을 드려 죄송합니다.</p>
+      <p>다시 요청 버튼을 눌러 재시도해주시기 바랍니다.</p>
+    </div>
+    <footer class="modal-card-foot">
+      <b-button @click.prevent="$emit('close')">닫기</b-button>
+    </footer>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "SendRequestError"
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/SendRequestSuccess.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="modal-card">
+    <header class="modal-card-head">
+      <p class="modal-card-title">요청을 성공적으로 전송하였습니다. 🎉</p>
+    </header>
+    <div class="modal-card-body">
+      <p>이용해주셔서 감사합니다.</p>
+      <p>빠른 시일내에 입력해주신 이메일로 연락드리겠습니다.</p>
+    </div>
+    <footer class="modal-card-foot">
+      <b-button @click.prevent="$emit('close')">닫기</b-button>
+    </footer>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "SendRequestSuccess"
+};
+</script>
+
+<style scoped></style>

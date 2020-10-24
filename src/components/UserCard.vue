a0ab512a7b7c7bafff5b2a1b9d1695ab16a97424
Rosie <rosie.yang@formakers.net>
2020-10-24 19:51:32 +1100

    style: UserCard 컴포넌트 스타일링
    https://trello.com/c/eqBTPY36

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="user-card">
-    <h3>{{ index + 1 }}</h3>
-    <p>성별 : {{ user.gender }} / 출생연도 : {{ user.birthday }}년 / 직업 : {{ user.job }}</p>
-    <p>선호 플랫폼 : {{ user.favoritePlatforms }}</p>
-    <p>선호 장르 : {{ user.favoriteGenres }} / 비선호 장르 : {{ user.leastFavoriteGenres }}</p>
-    <p>디바이스 : 제조사 - {{ user.device.manufacturer }}, 모델명 - {{ user.device.model }}, OS버전 - {{ user.device.osVersion }}</p>
-    <p>인생게임 : {{ user.lifeApps }}</p>
-    <p>월 과금규모 : {{ user.monthlyPayment }}만원</p>
-    <p>최근 접속일 : {{ user.activatedDate }}</p>
-    <p>총 게임플레이시간 : {{ user.totalPlayTime }}</p>
+  <div class="card user-card">
+    <div class="card-content">
+      <span class="index">{{ index + 1 }}</span>
+      <span class="user-profile">{{ user.gender }} / {{ user.birthday }}년생 / {{ user.job }}</span>
+      <p><img src="mobile.svg" /> <span class="field-title">제조사:</span> {{ user.device.manufacturer }} / <span class="field-title">모델명:</span> {{ user.device.model }} / <span class="field-title">OS버전:</span> {{ user.device.osVersion }}</p>
+      <p><span class="field-title">선호 플랫폼:</span> {{ user.favoritePlatforms }}</p>
+      <p><span class="field-title">선호 장르:</span> {{ user.favoriteGenres }} / <span class="field-title">비선호 장르:</span> {{ user.leastFavoriteGenres }}</p>
+      <p><span class="field-title">인생게임:</span> {{ user.lifeApps }}</p>
+      <p><span class="field-title">월 과금규모:</span> {{ user.monthlyPayment }}만원</p>
+      <p><span class="field-title">총 게임플레이시간:</span> {{ user.totalPlayTime }}</p>
+      <p><span class="field-title">최근 접속일:</span> {{ user.activatedDate }}</p>
+    </div>
   </div>
 </template>
 
@@ -19,9 +21,35 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .user-card {
-  width: 70vw;
+  width: 50vw;
   margin: 2rem auto;
+  border-radius: 10px;
+  text-align: start;
+
+  .index {
+    font-size: 1.7rem;
+    font-weight: 900;
+    margin-right: 1.5rem;
+  }
+
+  .user-profile {
+    font-family: "Do Hyeon", sans-serif;
+    font-size: 1.4rem;
+  }
+
+  p {
+    margin: 0.7rem auto;
+  }
+
+  img {
+    margin-bottom: -6px;
+  }
+
+  .field-title {
+    font-weight: bold;
+    margin-right: 5px;
+  }
 }
 </style>

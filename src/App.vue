c108657bc1722d9523a2923fe76e24c45133831f
Rosie Yang <rosie.junghwa@gmail.com>
2020-11-04 17:17:36 +0900

    style: primary 컬러 설정
    https://trello.com/c/7TFmJUMT

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,13 @@ export default {
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Do+Hyeon&family=Noto+Sans+KR:wght@400;700;900&family=Pacifico&display=swap');
 
+@import "~bulma/sass/utilities/_all";
+
+$primary: #00BFBA;
+
+@import "~bulma";
+@import "~buefy/src/scss/buefy";
+
 #app {
   font-family: "Noto Sans KR", sans-serif;
   -webkit-font-smoothing: antialiased;

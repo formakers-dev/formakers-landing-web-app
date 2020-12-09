e1c0a1191da8e73e9cc079cf83be34e766f28979
Rosie Yang and Yena Kim <pair+rosie.junghwa+yenarue@gmail.com>
2020-12-09 14:08:25 +0900

    feat: NavBar & 로그인 버튼 추가
    https://trello.com/c/RW45QSho

---
+++ src/components/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <b-navbar shadow>
+    <template slot="brand">
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        <img src="/formakers_logo.png" alt="ForMakers" />
+      </b-navbar-item>
+    </template>
+
+    <template slot="end">
+      <b-navbar-item tag="div">
+        <div class="buttons">
+          <div class="button is-primary is-small" @click="moveToLogin">로그인</div>
+        </div>
+      </b-navbar-item>
+    </template>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  methods: {
+    moveToLogin() {
+      this.$router.push("/login");
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

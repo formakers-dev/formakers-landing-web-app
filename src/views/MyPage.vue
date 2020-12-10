8394295dc4664fb67a16f55c288beea44e4202ed
Rosie Yang and Yena Kim <pair+rosie.junghwa+yenarue@gmail.com>
2020-12-10 17:29:42 +0900

    feat: 로그아웃 기능 추가
    https://trello.com/c/s4pS5BBs

--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -4,20 +4,26 @@
       <div class="column is-narrow">
         <b-menu class="menu">
           <b-menu-list>
-            <b-menu-item label="내 의뢰 목록" tag="router-link" to="/my-page/request-list"/>
-            <b-menu-item label="로그아웃" @click="onClickLogout"/>
+            <b-menu-item
+              label="내 의뢰 목록"
+              tag="router-link"
+              to="/my-page/request-list"
+            />
+            <b-menu-item label="로그아웃" @click="onClickLogout" />
           </b-menu-list>
         </b-menu>
       </div>
 
       <div class="column">
-        <router-view/>
+        <router-view />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import request from "@/common/utils/http";
+
 export default {
   name: "MyPage",
   data() {
@@ -25,9 +31,24 @@ export default {
       isSidebarOpened: true
     };
   },
-  methods:{
+  methods: {
     onClickLogout() {
-      console.log("logout!");
+      request
+        .post("/auth/logout")
+        .then(() => {
+          this.$buefy.toast.open({
+            type: "is-success",
+            message: "성공적으로 로그아웃되었습니다 👋"
+          });
+          this.$router.push("/");
+        })
+        .catch(err => {
+          console.error(err);
+          this.$buefy.toast.open({
+            type: "is-danger",
+            message: "에러가 발생했습니다. 다시 한 번 시도해주세요. 🙏"
+          });
+        });
     }
   }
 };

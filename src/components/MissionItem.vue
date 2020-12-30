47921b2d332e069c4dec8c264eec651f7884d4ef
Rosie Yang and Yena Kim <pair+rosie.junghwa+yenarue@gmail.com>
2020-12-30 15:51:10 +0900

    feat: 미션리스트 추가
    - style: 상세페이지 레이아웃 변경
    
    https://trello.com/c/P2mdpMpA

---
+++ src/components/MissionItem.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container">
+    <div class="box">
+      <div class="tags-container">
+        <div class="types-container">
+          <b-tag type="is-danger is-light">{{ types[mission.type] }}</b-tag>
+        </div>
+        <div
+          class="options-container"
+          v-for="option in mission.options.map(option => options[option])"
+          :key="option.index"
+        >
+          <b-tag type="is-info is-light">{{ option }}</b-tag>
+        </div>
+      </div>
+
+      <div class="content-container">
+        <p><strong>[{{ mission.order }}번째] {{ mission.title }}</strong></p>
+        <p v-if="mission.packageName">- 게임패키지명: {{ mission.packageName }}</p>
+        <p>- 미션참여 URL: {{ mission.action }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MissionItem",
+  props: ["mission"],
+  data() {
+    return {
+      options: {
+        mandatory: "필수",
+        repeatable: "여러번 참여 가능",
+        recheckable: "종료 후 열람 가능"
+      },
+      types: {
+        install: "설치형",
+        play: "플레이형",
+        survey: "설문형"
+      }
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.types-container,
+.options-container {
+  display: inline;
+}
+
+.tags-container {
+  span {
+    margin-right: 5px;
+  }
+}
+
+.content-container {
+  margin-top: 0.5rem;
+}
+</style>
--- src/views/BetaTest.vue
+++ src/views/BetaTest.vue
@@ -1,42 +1,51 @@
 <template>
-  <div class="container">
-    <div class="box">
-      <article class="media">
-        <div class="media-left">
-          <figure class="image is-64x64">
-            <img :src="betaTestInfo.iconImageUrl" alt="coverImage">
-          </figure>
+  <section>
+    <h1 class="title">상세페이지</h1>
+    <div class="header">
+      <div class="header-content">
+        <img :src="betaTestInfo.iconImageUrl" alt="iconImage">
+        <div class="header-text">
+          <p>{{ "#" + betaTestInfo.tags.join(" #") }}</p>
+          <p class="subtitle"><strong>{{ betaTestInfo.title }}</strong></p>
+
+          <p>{{ betaTestInfo.description }}</p>
+          <p>{{ testPeriod }}</p>
         </div>
-        <div class="media-content">
-          <div class="content">
-            <p>
-              <strong style="margin-right: 1rem">{{ betaTestInfo.title }}</strong>
-              <small>{{ betaTestInfo.tags.join(", ") }}</small>
-              <br>
-              {{ betaTestInfo.description }}
-              <br>
-              테스트기간: {{ testPeriod }}
-            </p>
-          </div>
-        </div>
-      </article>
+      </div>
     </div>
-  </div>
+
+    <div class="container">
+      <div class="missions-container">
+        <h2 class="subtitle">미션</h2>
+        <div class="mission-item"
+             v-for="mission in missions"
+             :key="mission._id">
+          <MissionItem :mission="mission"/>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import MissionItem from "@/components/MissionItem";
 import request from "@/common/utils/http";
 
 export default {
   name: "BetaTest",
+  components: {
+    MissionItem
+  },
   data() {
     return {
       betaTestInfo: {},
-      testPeriod: '',
+      testPeriod: "",
+      missions: []
     };
   },
   created() {
     this.fetchBetaTestInfo();
+    this.fetchMissions();
   },
   methods: {
     fetchBetaTestInfo() {
@@ -56,14 +65,58 @@ export default {
     },
     convertDate(date) {
       return `${date.slice(0, 4)}년 ${date.slice(5, 7)}월 ${date.slice(8, 10)}일`;
+    },
+    fetchMissions() {
+      request
+        .get(`/beta-tests/${this.$route.params.id}/missions`)
+        .then(res => {
+          this.missions = res.data.sort((a, b) => a.order > b.order ? 1 : -1);
+        })
+        .catch(err => {
+          console.error(err);
+          this.$buefy.toast.open({
+            type: "is-danger",
+            message: "에러가 발생했습니다. 다시 한 번 시도해주세요. 🙏"
+          });
+        });
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.container {
-  width: 70vw;
+section {
   margin: 2rem auto;
+
+  h1 {
+    text-align: center;
+  }
+}
+
+.header {
+  width: 100vw;
+  background-color: #F4F7FA;
+
+  .header-content {
+    width: 70%;
+    margin: 2rem auto;
+    padding: 2rem 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    img {
+      margin-right: 2rem;
+    }
+  }
+}
+
+.container {
+  margin: 2rem;
+
+  .subtitle {
+    text-align: center;
+    font-weight: bold;
+  }
 }
 </style>
